627ca307283b354308637110ac45e15c6f42ebbf
ban_ga_danh_che <hcmminer@gmail.com>
2024-10-31 17:10:18 +0700

    build extention

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: '../smart-ts-extention', // Đặt thư mục đầu ra là 'my-extension'
+  },
+  base: "./", // Đường dẫn tương đối để hỗ trợ file local
+});
